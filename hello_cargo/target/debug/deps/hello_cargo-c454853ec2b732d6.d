9e657326b31d4e30dd732546472706e4d76257e6
Mephilos <sth4628@hotmail.com>
2025-10-25 23:24:25 +0900

    git init

---
+++ hello_cargo/target/debug/deps/hello_cargo-c454853ec2b732d6.d
@@ -0,0 +1,5 @@
+/Users/teahosim/Dev/_Study/Rust/hello_cargo/target/debug/deps/hello_cargo-c454853ec2b732d6.d: src/main.rs
+
+/Users/teahosim/Dev/_Study/Rust/hello_cargo/target/debug/deps/hello_cargo-c454853ec2b732d6: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/teahosim/Dev/_Study/Rust/hello_cargo/target/debug/hello_cargo: /Users/teahosim/Dev/_Study/Rust/hello_cargo/src/main.rs
