78d9efc6e424b1de59987cd7036b3d47f617f05b
Mephilos <sth4628@hotmail.com>
2025-10-25 23:35:47 +0900

    fix, gitignore tracking for all files

--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/teahosim/Dev/_Study/Rust/hello_cargo/target/debug/hello_cargo: /Users/teahosim/Dev/_Study/Rust/hello_cargo/src/main.rs
--- hello_world/target/debug/deps/hello_world-96f488716a7cfd8d.d
+++
@@ -1,5 +0,0 @@
-/Users/teahosim/Dev/_Study/Rust/hello_world/target/debug/deps/hello_world-96f488716a7cfd8d.d: src/main.rs
-
-/Users/teahosim/Dev/_Study/Rust/hello_world/target/debug/deps/libhello_world-96f488716a7cfd8d.rmeta: src/main.rs
-
-src/main.rs:
