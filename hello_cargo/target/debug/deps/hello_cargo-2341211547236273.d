fbd2ef39ae3deb5509f5cf64937d2611646bf89d
Teaho Sim <teahosim@Teahoui-MacBookAir.local>
2025-10-25 23:35:47 +0900

    fix, gitignore tracking for all files

--- hello_cargo/target/debug/deps/hello_cargo-2341211547236273.d
+++
@@ -1,5 +0,0 @@
-/Users/teahosim/Dev/_Study/Rust/hello_cargo/target/debug/deps/hello_cargo-2341211547236273.d: src/main.rs
-
-/Users/teahosim/Dev/_Study/Rust/hello_cargo/target/debug/deps/libhello_cargo-2341211547236273.rmeta: src/main.rs
-
-src/main.rs:
